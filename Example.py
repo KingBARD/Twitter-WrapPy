b16db839b26ef1eb5624af91ff0c7a9c2d54c723
Brad <thisisbard@gmail.com>
2014-09-11 18:23:37 +1200

    Clean up

--- Example.py
+++ Example.py
@@ -5,20 +5,6 @@ def main():
 
     if Twitter.login('account', 'password'):
     	print('Signed into %s'%  Twitter.Account)
-        
-        
-    	# for a in Twitter.getFollowing('lol', True):
-    	# 	print(a)
-
-    	# Messages = ['Message 1','Message2']
-
-    	# for Message in Messages:
-    	# 	Twitter.directMessage('lol', Message)
-
-    	# Accs ['lol','a','op']	
-
-    	# for Acc in Accs:
-    	# 	Twitter.follow(Acc)	
     else:
     	print('Sign in failed')
 
