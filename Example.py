384e3333949bf9a3d01246f0fe79a3b69514f4c0
Brad <thisisbard@gmail.com>
2014-09-20 01:25:15 +1200

    Updated the example

--- Example.py
+++ Example.py
@@ -1,11 +1,11 @@
 import TwitterClient
 
 def main():
-    
+
     Twitter = TwitterClient.TwitterClient()
 
     if Twitter.login('account, password'):
-    	print('Signed into %s'%  Twitter.Account)
+    	print('Signed into %s' %  Twitter.Account)
     else:
     	print('Sign in failed')
     
